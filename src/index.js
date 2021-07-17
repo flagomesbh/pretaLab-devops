6af1f439b86d8e6e14334eb857f0f844cdda3b13
Flávia Gomes <flagomesbh@gmail.com>
2021-07-17 11:10:55 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World do Nosso tutorial');
+});
+
+module.exports = app;
+{"mode":"full","isActive":false}
