3df66674e4dcfab9c7c9dc71fc7cff05c19a0477
Flávia Gomes <flagomesbh@gmail.com>
2021-07-17 11:44:32 -0300

    Update index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -7,7 +7,7 @@ describe('Test the `/` path', () => {
       .get('/')
       .then((response) => {
         expect(response.statusCode).toBe(200);
-        expect(response.text).toBe('Hello World do Nosso tutorial');
+        expect(response.text).toBe('Congratulations, Flavia! It is alive!!');
       });
   });
 });
